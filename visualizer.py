ce050c1004040b72869bdc05c465e6bf04d812b0
AusarAlien <2415491839@qq.com>
2025-06-12 14:54:36 +0800

    wordcloud功能

--- visualizer.py
+++ visualizer.py
@@ -1,6 +1,6 @@
 from wordcloud import WordCloud
 #词云生成方法
-def generate_wordcloud(text, output_path="wordcloud.png"):
+def generate_wordcloud(text, output_path="wordclouds/wordcloud.png"):
     wc = WordCloud(
         font_path="msyh.ttc",  # 指定中文字体，若无可去掉此参数
         width=400, height=200,
